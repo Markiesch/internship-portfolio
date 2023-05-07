54e117f911440ee6e62fa0bd946ac43410cda3fb
Markiesch <marksmeel@gmail.com>
2023-05-07 16:36:04 +0200

    Fix: slide in delay applying to light/dark mode toggle

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function IndexPage() {
         <h1 className="slide-in text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
           Portfolio internship Ctrl Learning
         </h1>
-        <p className="slide-in max-w-[700px] pb-6 pt-4 text-lg text-muted-foreground delay-300  sm:text-xl">
+        <p className="slide-in slide-in-300 max-w-[700px] pb-6 pt-4 text-lg text-muted-foreground  sm:text-xl">
           This portfolio is to showcase the things a made during my second
           internship at Ctrl Learning
         </p>
@@ -20,7 +20,9 @@ export default function IndexPage() {
           <Link
             href="projects"
             rel="noreferrer"
-            className={buttonVariants({ size: "lg" }) + " slide-in delay-500"}
+            className={
+              buttonVariants({ size: "lg" }) + " slide-in slide-in-500"
+            }
           >
             Projecten
           </Link>
@@ -30,14 +32,14 @@ export default function IndexPage() {
             href={siteConfig.links.github}
             className={
               buttonVariants({ variant: "outline", size: "lg" }) +
-              " slide-in delay-700"
+              " slide-in slide-in-700"
             }
           >
             GitHub
           </Link>
         </div>
       </div>
-      <div className="slide-in delay-1000">
+      <div className="slide-in slide-in-900">
         <Technologies />
         <HomeProjects />
       </div>
